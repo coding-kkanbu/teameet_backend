14c7e1f30070c43a08b5e3fdee8d1a6e2e56888e
teacherSsamko <ssamko@kakao.com>
2022-01-16 15:47:39 +0900

    board&operation: admin: register models

--- board/admin.py
+++ board/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Board, Category
+from .models import Category, Comment, CommentLike, Post, PostLike, SogaetingOption, Tag
 
 admin.register(Category)
-admin.register(Board)
+admin.register(Post)
+admin.register(PostLike)
+admin.register(SogaetingOption)
+admin.register(Comment)
+admin.register(CommentLike)
+admin.register(Tag)
--- operation/admin.py
+++ operation/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import PostBlame
+from .models import CommentBlame, PostBlame
 
 admin.register(PostBlame)
+admin.register(CommentBlame)

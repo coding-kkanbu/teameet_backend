4a5590452204de83d2ea0c8e153a93ba2856a26e
teacherSsamko <ssamko@kakao.com>
2022-01-16 23:23:26 +0900

    operation: models: fix foreignkey

--- kkanbu/operation/models.py
+++ kkanbu/operation/models.py
@@ -2,14 +2,16 @@ from django.conf import settings
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
+from kkanbu.board.models import Comment, Post
+
 User = settings.AUTH_USER_MODEL
 
 
 class PostBlame(TimeStampedModel):
-    post = models.ForeignKey("board.Post", on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class CommentBlame(TimeStampedModel):
-    comment = models.ForeignKey("board.Comment", on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)

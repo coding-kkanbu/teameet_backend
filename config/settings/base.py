7a33171c67456d64eac472701b9b52e357ca2c06
hoetaekpro <hoetaek@student.snue.ac.kr>
2022-01-16 18:47:34 +0900

    mv accounts to kkanbu

--- config/settings/base.py
+++ config/settings/base.py
@@ -84,7 +84,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "kkanbu.users",
-    "accounts"
+    "kkanbu.accounts"
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps

c931acd98d0133f4bba1368a34112c1ff17ccd61
teacherSsamko <ssamko@kakao.com>
2022-01-16 15:40:28 +0900

    board: models: add other options

--- board/models.py
+++ board/models.py
@@ -14,6 +14,7 @@ class Category(models.Model):
 class Post(TimeStampedModel):
     title = models.CharField(max_length=128)
     category = models.ManyToManyField(Category)
+    tag = models.ManyToManyField("Tag", blank=True)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     is_show = models.BooleanField(default=True)
@@ -29,5 +30,72 @@ class Post(TimeStampedModel):
             models.UniqueConstraint(
                 fields=["title", "writer"],
                 name="unique title, writer",
+            )
+        ]
+
+
+class SogaetingOption(models.Model):
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+    region = models.CharField(max_length=30)
+    gender = models.PositiveIntegerField(default=1)
+    age = models.PositiveIntegerField(default=24)
+
+
+class PostLike(TimeStampedModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.post} | {self.user}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["post", "user"],
+                name="unique post, user",
+            )
+        ]
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(TimeStampedModel):
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_show = models.BooleanField(default=True)
+    secret = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    ip = models.GenericIPAddressField(null=True, blank=True)
+
+    def __str__(self):
+        return f"[{self.id}]{self.comment[:10]} | {self.writer}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["comment", "writer"],
+                name="unique comment, writer",
+            )
+        ]
+
+
+class CommentLike(TimeStampedModel):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.comment} | {self.user}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["comment", "user"],
+                name="unique comment, user",
             )
         ]

f132b7fde8af9d7714cc2bfc930e05b1564cde28
teacherSsamko <ssamko@kakao.com>
2022-01-16 23:24:25 +0900

    board & operation: fix admin
    - site.register

--- kkanbu/board/admin.py
+++ kkanbu/board/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 
 from .models import Category, Comment, CommentLike, Post, PostLike, SogaetingOption, Tag
 
-admin.register(Category)
-admin.register(Post)
-admin.register(PostLike)
-admin.register(SogaetingOption)
-admin.register(Comment)
-admin.register(CommentLike)
-admin.register(Tag)
+admin.site.register(
+    [Category, Comment, CommentLike, Post, PostLike, SogaetingOption, Tag]
+)
--- kkanbu/operation/admin.py
+++ kkanbu/operation/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 from .models import CommentBlame, PostBlame
 
-admin.register(PostBlame)
-admin.register(CommentBlame)
+admin.site.register(PostBlame)
+admin.site.register(CommentBlame)

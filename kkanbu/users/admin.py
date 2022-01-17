ff60d17330a5d9219c90f2a6f139e22b01e0c5f7
Ridealist <cjsn92@gmail.com>
2022-01-17 16:36:51 +0900

    customize User model

--- kkanbu/users/admin.py
+++ kkanbu/users/admin.py
@@ -1,34 +1,32 @@
 from django.contrib import admin
-from django.contrib.auth import admin as auth_admin
-from django.contrib.auth import get_user_model
-from django.utils.translation import gettext_lazy as _
+from kkanbu.users.models import User
+# from django.contrib.auth import admin as auth_admin
+# from django.utils.translation import gettext_lazy as _
+# from kkanbu.users.forms import UserChangeForm, UserCreationForm
 
-from kkanbu.users.forms import UserChangeForm, UserCreationForm
+admin.site.register(User)
 
-User = get_user_model()
+# @admin.register(User)
+# class UserAdmin(auth_admin.UserAdmin):
 
-
-@admin.register(User)
-class UserAdmin(auth_admin.UserAdmin):
-
-    form = UserChangeForm
-    add_form = UserCreationForm
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("name", "email")}),
-        (
-            _("Permissions"),
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                ),
-            },
-        ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
-    )
-    list_display = ["username", "name", "is_superuser"]
-    search_fields = ["name"]
+#     form = UserChangeForm
+#     add_form = UserCreationForm
+#     fieldsets = (
+#         (None, {"fields": ("username", "password")}),
+#         (_("Personal info"), {"fields": ("name", "email")}),
+#         (
+#             _("Permissions"),
+#             {
+#                 "fields": (
+#                     "is_active",
+#                     "is_staff",
+#                     "is_superuser",
+#                     "groups",
+#                     "user_permissions",
+#                 ),
+#             },
+#         ),
+#         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+#     )
+#     list_display = ["username", "name", "is_superuser"]
+#     search_fields = ["name"]

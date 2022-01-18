9d01068b0c9fb8994e9bcac17f429dd38b18e524
Junbo Koh <89024993+Ridealist@users.noreply.github.com>
2022-01-18 14:33:36 +0900

    Re-update models.py

--- kkanbu/users/models.py
+++ kkanbu/users/models.py
@@ -9,14 +9,14 @@ class User(AbstractUser):
     first_name = None  # type: ignore
     last_name = None  # type: ignore
     
-    nickname = models.CharField(_('nickname'), max_length=150, unique=True)
+    nickname = models.CharField(_("nickname"), max_length=150, unique=True)
     random_name = models.CharField(
-        _('random name'), max_length=150, blank=True, null=True
+        _("random name"), max_length=150, blank=True, null=True
     )
-    profile_image = models.ImageField(_('profile'), blank=True, null=True)
-    ip = models.GenericIPAddressField(_('user IP'), blank=True, null=True)
-    neis_email = models.EmailField(_('NEIS email address'), blank=True, null=True)
-    is_verify = models.BooleanField(_('NEIS email verified'), default=False)
+    profile_image = models.ImageField(_("profile"), blank=True, null=True)
+    ip = models.GenericIPAddressField(_("user IP"), blank=True, null=True)
+    neis_email = models.EmailField(_("NEIS email address"), blank=True, null=True)
+    is_verify = models.BooleanField(_("NEIS email verified"), default=False)
 
     def get_absolute_url(self):
         """Get url for user's detail view.

4808d172231e0d90911be9cbbfe7ae82e0115e41
ssamko <teacher.ssamko@gmail.com>
2022-01-18 21:37:53 +0900

    users.models: restore name

--- kkanbu/users/models.py
+++ kkanbu/users/models.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
 
-    name = None
+    name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None
     last_name = None
     nickname = models.CharField(_("nickname"), max_length=150, unique=True)

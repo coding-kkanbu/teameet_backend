28833646c820b46d4dd2145cd0b821ee4bca9c4f
ssamko <teacher.ssamko@gmail.com>
2022-01-18 22:37:13 +0900

    users.admin: add nickname into fieldsets

--- kkanbu/users/admin.py
+++ kkanbu/users/admin.py
@@ -14,7 +14,7 @@ class UserAdmin(auth_admin.UserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
     fieldsets = (
-        (None, {"fields": ("username", "password")}),
+        (None, {"fields": ("username", "password", "nickname")}),
         (_("Personal info"), {"fields": ("name", "email")}),
         (
             _("Permissions"),

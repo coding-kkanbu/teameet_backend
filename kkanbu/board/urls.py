20825fdb9139b2165ca21306037be58346a81581
Ridealist <cjsn92@gmail.com>
2022-01-26 00:22:21 +0900

    board: url post_router comment out & add urlpatterns

--- kkanbu/board/urls.py
+++ kkanbu/board/urls.py
@@ -1,16 +1,15 @@
-from django.urls import re_path
+from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
-from .views import CommentViewSet, PostCreateView, PostListView  # PostViewSet,
+from .views import CommentViewSet, MainListView  # PostViewSet,
 
 router = DefaultRouter()
 # router.register(r"post", PostViewSet)
 router.register(r"comment", CommentViewSet)
 
-
+app_name = "post"
 urlpatterns = [
-    re_path(r"^$", PostListView.as_view(), name="list"),
-    re_path(r"^create/$", PostCreateView.as_view(), name="create"),
+    url(r"^$", MainListView.as_view(), name="main"),
 ]
 
 urlpatterns += router.urls

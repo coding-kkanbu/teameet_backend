a0d674da654d545fbeada2cf03fb1354a259c3ea
Ridealist <cjsn92@gmail.com>
2022-01-29 10:56:21 +0900

    board: add permission_classes variables to View

--- kkanbu/board/views.py
+++ kkanbu/board/views.py
@@ -3,6 +3,7 @@ import logging
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .models import Category, Comment, Post
 from .serializers import BoardListSerializer, PostCreateSerializer, PostSerializer
@@ -21,11 +22,13 @@ logger = logging.getLogger(__name__)
 class BoardListView(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = BoardListSerializer
+    permission_classes = [AllowAny]
 
 
 class PostCreateView(CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostCreateSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         user = self.request.user

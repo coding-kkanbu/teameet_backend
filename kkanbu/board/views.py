ddc6dd89b0c2a5b96c2ccd9e07ff5c4f8c04a65d
Ridealist <cjsn92@gmail.com>
2022-01-29 10:38:09 +0900

    board: add PostCreateView

--- kkanbu/board/views.py
+++ kkanbu/board/views.py
@@ -2,10 +2,10 @@ import logging
 
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 
-from .models import Category, Comment
-from .serializers import BoardListSerializer, PostSerializer
+from .models import Category, Comment, Post
+from .serializers import BoardListSerializer, PostCreateSerializer, PostSerializer
 
 logger = logging.getLogger(__name__)
 
@@ -21,6 +21,15 @@ logger = logging.getLogger(__name__)
 class BoardListView(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = BoardListSerializer
+
+
+class PostCreateView(CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostCreateSerializer
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(writer=user)
 
 
 @extend_schema(

578138f67b47a803f7238aa1d95b27ddc9f7589e
Ridealist <cjsn92@gmail.com>
2022-01-29 10:38:55 +0900

    board: add urlpatterns to PostCreateView

--- kkanbu/board/urls.py
+++ kkanbu/board/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
-from .views import BoardListView, CommentViewSet  # PostViewSet,
+from .views import BoardListView, CommentViewSet, PostCreateView  # PostViewSet,
 
 router = DefaultRouter()
 # router.register(r"post", PostViewSet)
@@ -10,6 +10,7 @@ router.register(r"comment", CommentViewSet)
 app_name = "board"
 urlpatterns = [
     url(r"^$", BoardListView.as_view(), name="board-list"),
+    url(r"^create/$", PostCreateView.as_view(), name="create"),
 ]
 
 urlpatterns += router.urls

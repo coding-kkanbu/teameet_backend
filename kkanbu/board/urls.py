c5a4c98a159ae44b9ebcab41a8131afb23cab54e
Ridealist <cjsn92@gmail.com>
2022-01-29 17:38:56 +0900

    board: modify url patterns to BoardView

--- kkanbu/board/urls.py
+++ kkanbu/board/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
-from .views import BoardListView, CommentViewSet, PostCreateView  # PostViewSet,
+from .views import BoardView, CommentViewSet  # PostViewSet
 
 router = DefaultRouter()
 # router.register(r"post", PostViewSet)
@@ -9,8 +9,7 @@ router.register(r"comment", CommentViewSet)
 
 app_name = "board"
 urlpatterns = [
-    url(r"^$", BoardListView.as_view(), name="board-list"),
-    url(r"^create/$", PostCreateView.as_view(), name="create"),
+    url(r"^$", BoardView.as_view(), name="board"),
 ]
 
 urlpatterns += router.urls

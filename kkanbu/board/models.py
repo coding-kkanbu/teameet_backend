8e2ab4b25e16329fdc83f00388dcdc53dd562c92
Ridealist <cjsn92@gmail.com>
2022-02-06 22:58:03 +0900

    board: modify slug field not null constraint & add model method

--- kkanbu/board/models.py
+++ kkanbu/board/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils.text import slugify
 from django_extensions.db.models import TimeStampedModel
 
@@ -8,15 +9,18 @@ User = settings.AUTH_USER_MODEL
 
 class Category(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    slug = models.SlugField(null=True, allow_unicode=True)
+    slug = models.SlugField(null=False, allow_unicode=True)
 
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse("board:topic", kwargs={"slug": self.slug})
+
     def save(self, *args, **kwargs):
         if self.slug is None:
             self.slug = slugify(self.name, allow_unicode=True)
-        super().save(*args, **kwargs)
+        return super().save(*args, **kwargs)
 
 
 class Post(TimeStampedModel):

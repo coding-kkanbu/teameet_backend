e72213fc9a21e708f67ba35f7ef9d223b929e1ae
hoetaekpro <hoetaek@student.snue.ac.kr>
2022-02-13 14:56:54 +0900

    refactor: unique constraint for operation models

--- kkanbu/operation/models.py
+++ kkanbu/operation/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-from django_extensions.db.models import TimeStampedModel
+from django_extensions.db.models import TimeStampedModel, UniqueConstraint
 
 from kkanbu.board.models import Comment, Post
 
@@ -14,6 +14,11 @@ class PostBlame(TimeStampedModel):
     def __str__(self):
         return f"{self.post} blamed by {self.user}"
 
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=["post", "user"], name="unique_user_per_post"),
+        ]
+
 
 class CommentBlame(TimeStampedModel):
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
@@ -21,3 +26,8 @@ class CommentBlame(TimeStampedModel):
 
     def __str__(self):
         return f"{self.comment} blamed by {self.user}"
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=["comment", "user"], name="unique_user_per_post"),
+        ]

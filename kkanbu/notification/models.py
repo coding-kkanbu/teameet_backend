ec0c9f6337f432bc8a82fafc65e9767357dfdacd
Ridealist <cjsn92@gmail.com>
2022-02-18 21:18:43 +0900

    notification: make Notification model

--- kkanbu/notification/models.py
+++ kkanbu/notification/models.py
@@ -1,3 +1,39 @@
-# from django.db import models
+from django.conf import settings
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class Notification(TimeStampedModel):
+    COMMENT = "comment"
+    POSTLIKE = "postlike"
+    COMMENTLIKE = "commentlike"
+    POSTBLAME = "postblame"
+    COMMENTBLAME = "commentblame"
+
+    CHOICES = (
+        (COMMENT, "Comment"),
+        (POSTLIKE, "PostLike"),
+        (COMMENTLIKE, "CommentLike"),
+        (POSTBLAME, "PostBlame"),
+        (COMMENTBLAME, "CommentBlame"),
+    )
+
+    user_to = models.ForeignKey(
+        User, related_name="notification_to", on_delete=models.CASCADE
+    )
+    user_by = models.ForeignKey(
+        User, related_name="notification_by", on_delete=models.CASCADE
+    )
+    notification_type = models.CharField(max_length=20, choices=CHOICES)
+    extra_id = models.IntegerField(blank=True, null=True)
+    is_read = models.BooleanField(default=False)
+    url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f"[{self.notification_type} | {self.extra_id}]\
+            (To '{self.user_to.nickname}' By '{self.user_by.nickname}')"
+
+    class Meta:
+        ordering = ["-created"]

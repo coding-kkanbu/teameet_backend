2309c4478b72cde9822ec1f5ad8ee433e2de8373
Ridealist <cjsn92@gmail.com>
2022-03-02 03:02:59 +0900

    notification: modify admin-site view

--- kkanbu/notification/admin.py
+++ kkanbu/notification/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 
 from .models import Notification
 
-admin.site.register(Notification)
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = (
+        "notification_type",
+        "recipient",
+        "sender",
+        "is_read",
+    )
+    list_filter = (
+        "notification_type",
+        "is_read",
+        "created",
+    )
+
+
+admin.site.register(Notification, NotificationAdmin)

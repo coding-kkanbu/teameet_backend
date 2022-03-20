bb33643be5ab5dedd1c02b92a3869b1fcbbce3ef
Ridealist <cjsn92@gmail.com>
2022-03-20 14:02:22 +0900

    board/operation: modify admin.py

--- kkanbu/board/admin.py
+++ kkanbu/board/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from .models import Category, Comment, CommentLike, Post, PostLike, SogaetingOption, Tag
+from .models import Category, Comment, Post, SogaetingOption, Tag
 
-admin.site.register(
-    [Category, Comment, CommentLike, Post, PostLike, SogaetingOption, Tag]
-)
+admin.site.register([Category, Comment, Post, SogaetingOption, Tag])
--- kkanbu/operation/admin.py
+++ kkanbu/operation/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-from .models import CommentBlame, PostBlame
+from .models import CommentBlame, CommentLike, PostBlame, PostLike
 
-admin.site.register(PostBlame)
-admin.site.register(CommentBlame)
+admin.site.register([CommentLike, PostLike, CommentBlame, PostBlame])

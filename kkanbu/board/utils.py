413bb9448e59d5dd7d4a7edc937df444d6666c4c
Ridealist <cjsn92@gmail.com>
2022-03-27 21:55:00 +0900

    board: Add method for CRUD action & Add method for blame operation

---
+++ kkanbu/board/utils.py
@@ -0,0 +1,16 @@
+from rest_framework.exceptions import APIException
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(",")[0]
+    else:
+        ip = request.META.get("REMOTE_ADDR")
+    return ip
+
+
+class UniqueBlameError(APIException):
+    status_code = 400
+    default_detail = "신고는 한번 밖에 할 수 없습니다."
+    default_code = "unique_blame_constraint"

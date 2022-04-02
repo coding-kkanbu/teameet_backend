e963805cffceffe914fef82668544f181e1fcf6d
Ridealist <cjsn92@gmail.com>
2022-04-03 00:09:02 +0900

    notification: Edit models view_name & add serializer custom fields

--- kkanbu/notification/serializers.py
+++ kkanbu/notification/serializers.py
@@ -1,9 +1,31 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    HyperlinkedIdentityField,
+    ModelSerializer,
+    SerializerMethodField,
+)
 
 from .models import Notification
 
 
 class NotificationSerializer(ModelSerializer):
+    sender_info = SerializerMethodField()
+    url = HyperlinkedIdentityField(view_name="api:Notification-detail")
+
     class Meta:
         model = Notification
-        fields = "__all__"
+        fields = [
+            "id",
+            "notification_type",
+            "sender_info",
+            "message",
+            "is_read",
+            "created",
+            "url",
+        ]
+
+    def get_sender_info(self, obj):
+        if obj.notification_type == "comment":
+            sender_info = obj.sender.writer.nickname
+        else:
+            sender_info = obj.sender.user.nickname
+        return sender_info

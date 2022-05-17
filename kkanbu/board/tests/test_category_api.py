b53868164ed51ee695d0bf1fd9441c22d29d5d4b
Ridealist <cjsn92@gmail.com>
2022-05-17 23:47:52 +0900

    board/category: Edit test codes & Implement help function

--- kkanbu/board/tests/test_category_api.py
+++ kkanbu/board/tests/test_category_api.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 
 from kkanbu.board.models import Category, Post
+from kkanbu.board.utils import url_with_query
 
 CATEGORY_LIST_URL = reverse("api:category-list")
 CATEGORY_DETAIL_URL = reverse("api:category-detail", args=["test1"])
@@ -40,16 +41,18 @@ class CategoryViewSetAPITest(APITestCase):
         self.client.force_authenticate(user=self.user)
 
     def test_retrieve_category_success(self):
-        """Test user allowed GET method"""
+        """Test user allowed GET method & received appropriate data"""
         res = self.client.get(CATEGORY_DETAIL_URL)
-
+        res_page2 = self.client.get(url_with_query(CATEGORY_DETAIL_URL, page=2))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data["results"][0]["title"], "Post_0")
+        self.assertEqual(res.data["results"][9]["content"], "Post_9_content")
+        self.assertEqual(res_page2.data["results"][-1]["title"], "Post_14")
 
     def test_create_category_not_allowed(self):
         """Test user not allowed POST method"""
         payload = {"app": "Topic", "name": "테스트2", "slug": "test2"}
         res = self.client.post(CATEGORY_LIST_URL, payload, format="json")
-
         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_update_category_not_allowed(self):
@@ -57,14 +60,12 @@ class CategoryViewSetAPITest(APITestCase):
         payload = {"name": "테스트2", "slug": "test2"}
         res_1 = self.client.put(CATEGORY_DETAIL_URL, payload, format="json")
         res_2 = self.client.patch(CATEGORY_DETAIL_URL, payload, format="json")
-
         self.assertEqual(res_1.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
         self.assertEqual(res_2.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_delete_category_not_allowed(self):
         """Test user not allowed DELETE method"""
         res = self.client.delete(CATEGORY_DETAIL_URL)
-
         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_recent_posts_GET_five_object(self):
@@ -87,18 +88,17 @@ class CategoryViewSetAPITest(APITestCase):
         self.assertTrue(post.title not in res_2.data)
         self.assertTrue(post.content not in res_2.data)
 
-        res_3 = self.client.get(CATEGORY_DETAIL_URL + "?page=2")
+        res_3 = self.client.get(url_with_query(CATEGORY_DETAIL_URL, page=2))
         self.assertTrue(post.title not in res_3.data)
         self.assertTrue(post.content not in res_3.data)
 
     def test_pagination_is_ten(self):
         res = self.client.get(CATEGORY_DETAIL_URL)
-
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data["results"]), 10)
 
     def test_lists_all_post_object_per_category(self):
         """Get second page and confirm it has (exactly) remaining 5 items"""
-        res = self.client.get(CATEGORY_DETAIL_URL + "?page=2")
+        res = self.client.get(url_with_query(CATEGORY_DETAIL_URL, page=2))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data["results"]), 5)

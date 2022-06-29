403f8cf43dad83a2c052f8d122c25d11439ac65a
Ridealist <cjsn92@gmail.com>
2022-06-30 02:40:59 +0900

    feat/auth: Delete 3rd party apps' urls.

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,6 @@ from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -34,12 +33,8 @@ urlpatterns += [
     ),
     # API base url
     path("api/", include("config.api_router")),
-    # DRF auth token
-    path("auth-token/", obtain_auth_token),
-    path("accounts/", include("dj_rest_auth.urls")),
     # Local apps
     path("accounts/", include("kkanbu.accounts.urls")),
-    path("accounts/", include("allauth.urls")),
 ]
 
 if settings.DEBUG:

3514c9ef23419cef0b116b81899e6a75df6d699f
Ridealist <cjsn92@gmail.com>
2022-06-30 02:38:57 +0900

    feat/auth: Edit settings for authentication & accounts App.

--- config/settings/local.py
+++ config/settings/local.py
@@ -67,7 +67,8 @@ CELERY_TASK_EAGER_PROPAGATES = True
 # Your stuff...
 # ------------------------------------------------------------------------------
 
-BASE_URL = "http://127.0.0.1:8000/"
+# TODO Production 단계에서 CALLBACK URI 수정
+LOCAL_BASE_URL = "http://127.0.0.1:8000/"
 
-GOOGLE_CALLBACK_URI = BASE_URL + "accounts/google/login/callback/"
-KAKAO_CALLBACK_URI = BASE_URL + "accounts/kakao/login/callback/"
+GOOGLE_CALLBACK_URI = LOCAL_BASE_URL + "accounts/google/login/callback/"
+KAKAO_CALLBACK_URI = LOCAL_BASE_URL + "accounts/kakao/login/callback/"

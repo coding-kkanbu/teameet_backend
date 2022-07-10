ba92dc3631b337e4cdb82db18d4f68ed05d54619
Ridealist <cjsn92@gmail.com>
2022-07-10 23:25:38 +0900

    Edit Userserializer.

--- kkanbu/users/api/serializers.py
+++ kkanbu/users/api/serializers.py
@@ -5,10 +5,34 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    post_n = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ["username", "email", "url"]
+        fields = [
+            "id",
+            "username",
+            "email",
+            "date_joined",
+            "random_name",
+            "profile_image",
+            "neis_email",
+            "is_verify",
+            "post_n",
+            "url",
+        ]
+
+        read_only_fields = [
+            "email",
+            "date_joined",
+            "neis_email",
+            "is_verify",
+        ]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"}
         }
+
+    def get_post_n(self, obj):
+        return obj.post_set.count()

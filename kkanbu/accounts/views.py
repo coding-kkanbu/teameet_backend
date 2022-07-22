1713daa149176f00bcb9c2f7835e475683d02251
Ridealist <cjsn92@gmail.com>
2022-07-22 09:41:32 +0900

    feat/auth: Apply swagger UI extend schema for accounts app.

--- kkanbu/accounts/views.py
+++ kkanbu/accounts/views.py
@@ -1,18 +1,18 @@
 from allauth.socialaccount.models import SocialApp
 from allauth.socialaccount.providers.google import views as google_views
-from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.kakao import views as kakao_views
-from allauth.socialaccount.providers.kakao.views import KakaoOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
 from django.conf import settings
 from django.utils.http import urlencode
+from drf_spectacular.utils import extend_schema
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 
 # for backend test > should be implemented on frontend
+@extend_schema(tags=["accounts"])
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def get_callback(request):
@@ -20,10 +20,7 @@ def get_callback(request):
     return Response({"code": code})
 
 
-google_oauth2_login = google_views.oauth2_login
-kakao_oauth2_login = kakao_views.oauth2_login
-
-
+@extend_schema(tags=["accounts"])
 def get_redirect_url(adapter, provider_name, callback_url, scope):
     app = SocialApp.objects.get(provider=provider_name)
     params = {
@@ -36,6 +33,7 @@ def get_redirect_url(adapter, provider_name, callback_url, scope):
     return "{}?{}".format(adapter.authorize_url, urlencode(params).replace("%2B", "+"))
 
 
+@extend_schema(tags=["accounts"])
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def get_google_redirect_url(request):
@@ -45,11 +43,12 @@ def get_google_redirect_url(request):
         scope = None
 
     url = get_redirect_url(
-        GoogleOAuth2Adapter, "google", settings.GOOGLE_CALLBACK_URI, scope
+        google_views.GoogleOAuth2Adapter, "google", settings.GOOGLE_CALLBACK_URI, scope
     )
     return Response({"url": url})
 
 
+@extend_schema(tags=["accounts"])
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def get_kakao_redirect_url(request):
@@ -58,11 +57,12 @@ def get_kakao_redirect_url(request):
     except KeyError:
         scope = None
     url = get_redirect_url(
-        KakaoOAuth2Adapter, "kakao", settings.KAKAO_CALLBACK_URI, scope
+        kakao_views.KakaoOAuth2Adapter, "kakao", settings.KAKAO_CALLBACK_URI, scope
     )
     return Response({"url": url})
 
 
+@extend_schema(tags=["accounts"])
 class GoogleLogin(SocialLoginView):
     adapter_class = google_views.GoogleOAuth2Adapter
     client_class = OAuth2Client
@@ -70,6 +70,7 @@ class GoogleLogin(SocialLoginView):
     callback_url = settings.GOOGLE_CALLBACK_URI
 
 
+@extend_schema(tags=["accounts"])
 class KakaoLogin(SocialLoginView):
     adapter_class = kakao_views.KakaoOAuth2Adapter
     client_class = OAuth2Client

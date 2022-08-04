a68437bb130f8726cfc60acdeaf3e34eb9acfce9
hoetaek <hoetaek@student.snue.ac.kr>
2022-08-04 23:29:52 +0900

    fix: add default for env

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,5 +1,3 @@
-from dotenv import load_dotenv
-
 from .base import *  # noqa
 from .base import env
 
@@ -33,13 +31,12 @@ EMAIL_BACKEND = env(
     "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend"
 )
 
-load_dotenv()
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="sample_email_host_user")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="sample_email_host_password")
 DEFAULT_FROM_EMAIL = "teameet"
 
 # WhiteNoise

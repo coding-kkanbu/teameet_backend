e72b63183eec2d5ab697562d69e6a6aeaedf32f2
Ridealist <cjsn92@gmail.com>
2022-08-07 21:02:39 +0900

    chore: resolve test confict.

--- kkanbu/users/tests/test_tasks.py
+++ kkanbu/users/tests/test_tasks.py
@@ -1,5 +1,6 @@
 import pytest
 from celery.result import EagerResult
+from django.contrib.auth import get_user_model
 
 from kkanbu.users.tasks import get_users_count
 from kkanbu.users.tests.factories import UserFactory
@@ -9,6 +10,7 @@ pytestmark = pytest.mark.django_db
 
 def test_user_count(settings):
     """A basic test to execute the get_users_count Celery task."""
+    get_user_model().objects.all().delete()
     UserFactory.create_batch(3)
     settings.CELERY_TASK_ALWAYS_EAGER = True
     task_result = get_users_count.delay()

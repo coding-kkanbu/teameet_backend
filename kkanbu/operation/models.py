c91660dfff19785bb103605d99a6d20a8e39919d
Ridealist <cjsn92@gmail.com>
2022-08-07 20:20:59 +0900

    feat/operation: Add queryset filter & retrieve func.

--- kkanbu/operation/models.py
+++ kkanbu/operation/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.db.models.constraints import UniqueConstraint
+from django.urls import reverse
 from django_extensions.db.models import TimeStampedModel
 
 from kkanbu.board.models import Comment, Post
@@ -12,9 +13,6 @@ class PostLike(TimeStampedModel):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f"{self.post} liked by {self.user}"
-
     class Meta:
         constraints = [
             UniqueConstraint(
@@ -23,13 +21,22 @@ class PostLike(TimeStampedModel):
             )
         ]
 
+    def __str__(self):
+        return f"{self.post} liked by {self.user}"
+
+    def get_absolute_url(self):
+        app = self.post.category.app
+        if app == "Topic":
+            return reverse("api:Topic-detail", kwargs={"pk": self.post.pk})
+        elif app == "PitAPat":
+            return reverse("api:PitAPat-detail", kwargs={"pk": self.post.pk})
+        else:
+            return None
+
 
 class CommentLike(TimeStampedModel):
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.comment} liked by {self.user}"
 
     class Meta:
         constraints = [
@@ -38,6 +45,12 @@ class CommentLike(TimeStampedModel):
                 name="unique_commentlike",
             )
         ]
+
+    def __str__(self):
+        return f"{self.comment} liked by {self.user}"
+
+    def get_absolute_url(self):
+        return reverse("api:Comment-detail", kwargs={"pk": self.comment.pk})
 
 
 class PostBlame(TimeStampedModel):
--- kkanbu/operation/views.py
+++ kkanbu/operation/views.py
@@ -1,6 +1,10 @@
+from django.shortcuts import redirect
 from drf_spectacular.utils import extend_schema
-from rest_framework.mixins import CreateModelMixin, ListModelMixin
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin
+from rest_framework.permissions import IsAdminUser
 from rest_framework.viewsets import GenericViewSet
+
+from kkanbu.users.models import User
 
 from .models import CommentBlame, CommentLike, PostBlame, PostLike
 from .serializers import (
@@ -14,21 +18,37 @@ from .serializers import (
 @extend_schema(tags=["operation"])
 class PostLikeViewSet(
     GenericViewSet,
-    CreateModelMixin,
     ListModelMixin,
+    RetrieveModelMixin,
 ):
     serializer_class = PostLikeSerializer
     queryset = PostLike.objects.all()
+
+    def get_queryset(self):
+        assert isinstance(self.request.user, User)
+        return self.queryset.filter(user=self.request.user)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        return redirect(instance.get_absolute_url())
 
 
 @extend_schema(tags=["operation"])
 class CommentLikeViewSet(
     GenericViewSet,
-    CreateModelMixin,
     ListModelMixin,
+    RetrieveModelMixin,
 ):
     serializer_class = CommentLikeSerializer
     queryset = CommentLike.objects.all()
+
+    def get_queryset(self):
+        assert isinstance(self.request.user, User)
+        return self.queryset.filter(user=self.request.user)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        return redirect(instance.get_absolute_url())
 
 
 @extend_schema(tags=["operation"])
@@ -39,6 +59,7 @@ class PostBlameViewSet(
 ):
     serializer_class = PostBlameSerializer
     queryset = PostBlame.objects.all()
+    permission_classes = [IsAdminUser]
 
 
 @extend_schema(tags=["operation"])
@@ -49,3 +70,4 @@ class CommentBlameViewSet(
 ):
     serializer_class = CommentBlameSerializer
     queryset = CommentBlame.objects.all()
+    permission_classes = [IsAdminUser]

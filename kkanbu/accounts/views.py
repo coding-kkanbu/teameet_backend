1c44c3e6537e64c2a955d1e7796f198b5765993a
hoetaek <hoetaek@student.snue.ac.kr>
2022-08-09 14:14:41 +0900

    refactor: permission is already in default

--- kkanbu/accounts/views.py
+++ kkanbu/accounts/views.py
@@ -16,7 +16,7 @@ from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from kkanbu.accounts.serializers import (
@@ -96,7 +96,6 @@ class KakaoLogin(SocialLoginView):
 
 
 class VerifyNeisEmail(GenericAPIView):
-    permission_classes = [IsAuthenticated]
     serializer_class = VerifyNeisEmailSerializer
 
     def post(self, request):

96917ac6aa42dec67603a292e4860b4d63a0c67e
Ridealist <cjsn92@gmail.com>
2022-08-28 16:43:44 +0900

    Category filtering for Topic app only

--- kkanbu/board/views.py
+++ kkanbu/board/views.py
@@ -180,6 +180,11 @@ class CategoryViewSet(ReadOnlyModelViewSet):
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset().filter(app="Topic")
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         queryset = instance.post_set.filter(is_show=True)

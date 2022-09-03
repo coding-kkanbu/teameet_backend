18857aecd991ead174e8c0acf499e3931f1bfa80
Ridealist <cjsn92@gmail.com>
2022-09-04 05:01:18 +0900

    fix/model: Add constraints and Edit choice fields.

--- kkanbu/board/models.py
+++ kkanbu/board/models.py
@@ -1,20 +1,25 @@
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+from django.db.models.functions import Length
 from django.utils.translation import gettext_lazy as _
 from django_extensions.db.models import TimeStampedModel
 from taggit.managers import TaggableManager
 
 from kkanbu.notification.models import Notification
 
+models.CharField.register_lookup(Length)
+models.TextField.register_lookup(Length)
+
 User = settings.AUTH_USER_MODEL
 
 
-class Category(models.Model):
-    class AppType(models.TextChoices):
-        TOPIC = "Topic", _("토픽")
-        PITAPAT = "PitAPat", _("두근두근")
+class AppType(models.TextChoices):
+    TOPIC = "Topic", _("토픽")
+    PITAPAT = "PitAPat", _("두근두근")
 
+
+class Category(models.Model):
     app = models.CharField(
         max_length=30, choices=AppType.choices, default=AppType.TOPIC
     )
@@ -22,7 +27,15 @@ class Category(models.Model):
     slug = models.SlugField(max_length=50, allow_unicode=True, unique=True)
 
     def __str__(self):
-        return f"{self.app} | {self.name}"
+        return f"[{self.id}]{AppType(self.app).label} | {self.name} | {self.slug}"
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(app__in=AppType.values),
+                name="%(app_label)s_%(class)s_app_type_valid",
+            )
+        ]
 
 
 class Post(TimeStampedModel):
@@ -39,32 +52,45 @@ class Post(TimeStampedModel):
     def __str__(self):
         return f"[{self.id}]{self.title} | {self.writer}"
 
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(title__length__gte=4),
+                name="%(app_label)s_%(class)s_title_length_gte_4",
+            ),
+            models.CheckConstraint(
+                check=models.Q(content__length__gte=4),
+                name="%(app_label)s_%(class)s_content_length_gte_4",
+            ),
+        ]
+
+
+class RegionType(models.TextChoices):
+    SEOUL = "서울"
+    BUSAN = "부산"
+    DAEGU = "대구"
+    INCHEON = "인천"
+    GWANGJU = "광주"
+    DAEJEON = "대전"
+    ULSAN = "울산"
+    SEJONG = "세종"
+    GYEONGGI = "경기"
+    GANGWON = "강원"
+    CHUNGBUK = "충북"
+    CHUNGNAM = "충남"
+    JEONBUK = "전북"
+    JEONNAM = "전남"
+    GYEONGBUK = "경북"
+    GYEONGNAM = "경남"
+    JEJU = "제주"
+
+
+class GenderType(models.IntegerChoices):
+    MALE = 1, _("남자")
+    FEMALE = 2, _("여자")
+
 
 class SogaetingOption(models.Model):
-    class RegionType(models.TextChoices):
-        SEOUL = "Seoul", _("서울")
-        BUSAN = "Busan", _("부산")
-        DAEGU = "Daegu", _("대구")
-        INCHEON = "Incheon", _("인천")
-        GWANGJU = "Gwangju", _("광주")
-        DAEJEON = "Daejeon", _("대전")
-        ULSAN = "Ulsan", _("울산")
-        SEJONG = "Sejong", _("세종")
-
-        GYEONGGI = "Gyeonggi", _("경기")
-        GANGWON = "Gangwon", _("강원")
-        CHUNGBUK = "Chungbuk", _("충북")
-        CHUNGNAM = "Chungnam", _("충남")
-        JEONBUK = "jeonbuk", _("전북")
-        JEONNAM = "Jeonnam", _("전남")
-        GYEONGBUK = "Gyeongbuk", _("경북")
-        GYEONGNAM = "Gyeongnam", _("경남")
-        JEJU = "Jeju", _("제주")
-
-    class GenderType(models.IntegerChoices):
-        MALE = 1, _("남자")
-        FEMALE = 2, _("여자")
-
     post = models.OneToOneField(
         Post, on_delete=models.CASCADE, primary_key=True, related_name="sogaetingoption"
     )
@@ -74,7 +100,23 @@ class SogaetingOption(models.Model):
     connected = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.post}  ||  {self.region} - {self.gender} - {self.age}"
+        return f"{self.post} || {self.region} - {GenderType(self.gender).label} - {self.age}"
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(region__in=RegionType.values),
+                name="%(app_label)s_%(class)s_region_type_valid",
+            ),
+            models.CheckConstraint(
+                check=models.Q(gender__in=GenderType.values),
+                name="%(app_label)s_%(class)s_gender_type_valid",
+            ),
+            models.CheckConstraint(
+                check=models.Q(age__gte=20),
+                name="%(app_label)s_%(class)s_age_gte_20",
+            ),
+        ]
 
 
 class Comment(TimeStampedModel):
@@ -96,6 +138,12 @@ class Comment(TimeStampedModel):
 
     class Meta:
         ordering = ("created",)
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(comment__length__gte=4),
+                name="%(app_label)s_%(class)s_comment_length_gte_4",
+            ),
+        ]
 
     def __str__(self):
         return f"[{self.id}]{self.comment[:10]} | {self.writer}"
